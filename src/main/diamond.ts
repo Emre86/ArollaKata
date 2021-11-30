42a331dde140ed0db8076eb8ec30e694fe007cca
Emre86 <emre.gun86@gmail.com>
2021-11-30 16:27:28 +0100

    Refactor

--- src/main/diamond.ts
+++ src/main/diamond.ts
@@ -2,32 +2,31 @@ export class Diamond {
 
 
     range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step));
-    getDiamond(letter: string) {
+    getDiamond(letter: string): string {
         const a = "A".charCodeAt(0);
         const l = letter.charCodeAt(0);
 
         const spaceExtern = this.range(0, (l - a), 1);
+        spaceExtern.reverse();
+
         const spaceIntern = this.range(1, (l - a) * 2 - 1, 2);
         spaceIntern.unshift(0);
 
-        let bottomDiamond = '';
-        for (let i = 1; i < spaceExtern.length; i++) {
-            const lineDiamond = this.generateLineDiamond(String.fromCharCode(l - i), spaceExtern[i], spaceIntern[spaceIntern.length - i - 1]);
-            bottomDiamond = bottomDiamond.concat(lineDiamond);
-        }
-
-        spaceExtern.reverse();
-
-        let topDiamond = '';
+        const top: Array<string> = [];
         for (let i = 0; i < spaceExtern.length; i++) {
             const lineDiamond = this.generateLineDiamond(String.fromCharCode(a + i), spaceExtern[i], spaceIntern[i]);
-            topDiamond = topDiamond.concat(lineDiamond);
+            top.push(lineDiamond);
         }
+
+        const bottom: Array<string> = [...top].reverse();
+        bottom.shift();
+        const topDiamond = top.join("").toString();
+        const bottomDiamond = bottom.join("").toString();
 
         return `${topDiamond}${bottomDiamond}`;
     }
 
-    generateLineDiamond(letter: string, spaceExtern: number, spaceIntern: number) {
+    generateLineDiamond(letter: string, spaceExtern: number, spaceIntern: number): string {
         const extern: string = " ".repeat(spaceExtern);
         const intern: string = " ".repeat(spaceIntern);
         if (spaceIntern === 0) {
